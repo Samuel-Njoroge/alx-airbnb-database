03d606d9d7f6d396ba1cb24cb15e44b3a1daa868
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-05-18 12:47:30 +0300

    aggregation queries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -12,11 +12,17 @@ ORDER BY total_bookings DESC;
 
 -- Total numbe of bookings
 SELECT 
-    p.property_id,
-    p.name,
-    COUNT(b.booking_id) AS total_bookings,
-    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
-FROM property p
-LEFT JOIN booking b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name
-ORDER BY booking_rank;
+    property_id,
+    name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS booking_rank
+FROM (
+    SELECT 
+        p.property_id,
+        p.name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM Property p
+    LEFT JOIN Booking b ON p.property_id = b.property_id
+    GROUP BY p.property_id, p.name
+) AS property_bookings;
+
