f9179f6f3caabac85ad44627381088ddcd91a636
Samuel-Njoroge <njorogekiragu8@gmail.com>
2025-05-18 14:27:34 +0300

    indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -13,3 +13,16 @@ CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_property_location ON Property(location);
 CREATE INDEX idx_property_pricepernight ON Property(pricepernight);
 CREATE INDEX idx_property_created_at ON Property(created_at);
+
+
+-- Filter Bookings by Status and Date Range
+EXPLAIN ANALYZE
+SELECT * FROM Booking
+WHERE status = 'confirmed'
+AND created_at BETWEEN '2024-01-01' AND '2024-12-31';
+
+-- Join User and Booking by `user_id`
+EXPLAIN ANALYZE
+SELECT u.first_name, u.email, b.total_price
+FROM "User" u
+JOIN Booking b ON u.user_id = b.user_id;
